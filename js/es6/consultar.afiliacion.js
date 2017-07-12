2a370a556208df4f6ff5f9f22a9804a5a65ef2f8
Carlos E. Peña <gesaodin@gmail.com>
2017-07-12 11:50:03 -0400

    #Familiares

--- js/es6/consultar.afiliacion.js
+++ js/es6/consultar.afiliacion.js
@@ -1,5 +1,6 @@
 function Buscar(){
   $("#_cargando").show()
+  $("#_imgfamiliar").attr("src", "images/ndisponible.jpg");
   var xhttp = new XMLHttpRequest();
   xhttp.open("GET", "http://192.168.6.45:8080/ipsfa/api/militar/crud/" + $("#_cedula").val());
   xhttp.onreadystatechange = function() {
@@ -23,19 +24,22 @@ function Buscar(){
           $("#_img").attr("src", url);
           $("#txtnombre").val(DB.nombreprimero + ' ' + DB.nombresegundo);
           $("#txtapellido").val(DB.apellidoprimero + ' ' + DB.apellidosegundo);
-          $("#txtnacimiento").val(DB.fechanacimiento);
+          $("#txtnacimiento").val(ConvertirFechaHumana(DB.fechanacimiento));
           $("#cmbsexo").val(DB.sexo);
           $("#cmbedocivil").val(DB.estadocivil);
           $("#cmbcomponente").val(militar.Componente.abreviatura);
-          $("#_fingreso").html(militar.fingreso);
-          $("#_fascenso").html(militar.fascenso);
+          $("#_fingreso").html(ConvertirFechaHumana(militar.fingreso));
+          $("#_fascenso").html(ConvertirFechaHumana(militar.fascenso));
           let i = 1;
           $.each(militar.Familiar, function (c, v){
             let DBF = v.Persona.DatoBasico;
             let cedula  = DBF.cedula;
             let nombre = DBF.apellidoprimero + ' ' + DBF.apellidosegundo + ' ' + DBF.nombreprimero + ' ' + DBF.nombresegundo;
             let parentesco = v.parentesco;
-
+            mil = nombre;
+            if (v.esmilitar == true){
+              mil = '<font color="#0E6626"><i class="fa fa-fw fa-male"></i></font>&nbsp;'  + nombre;
+            }
             ok = '<font color="#blue"><i class="fa fa-fw fa-close"></i></font>';
             if (v.beneficio == true){
               ok = '<font color="#green"><i class="fa fa-fw fa-check"></i></font>';
@@ -44,11 +48,24 @@ function Buscar(){
             t.row.add ([
               i++,
               cedula,
-              nombre,
+              mil,
               parentesco,
-              ok
+              ok,
+              DBF.fechanacimiento
             ]).draw(false);
-          })
+
+
+          });
+
+          t.column(5).visible(false);
+          $('#tblresultados tbody').on('click', 'tr', function () {
+              var data = t.row(this).data();
+              urlf = "http://192.168.12.198/imagenes/imagenes/" +  data[1] + ".jpg";
+              $("#_imgfamiliar").attr("src", urlf);
+              $("#_ffnacimiento").html(ConvertirFechaHumana(data[5]));
+              $("#_fcedula").html("C.I: V- " + data[1]);
+
+          });
 
         }
 
@@ -57,4 +74,12 @@ function Buscar(){
 
   };
   xhttp.send();
+}
+
+
+function ConvertirFechaHumana(f){
+  fe = f.substr(0,10);
+  fa = fe.split("-");
+  return fa[2] + "/" + fa[1] + "/" + fa[0];
+
 }
