d46a0dc0c740348064a0bb4c7a1368d5459f58ce
Carlos E. Peña <gesaodin@gmail.com>
2017-07-20 12:22:14 -0400

    Declaracion de Funciones

---
+++ js/es6/utilidad.js
@@ -0,0 +1,40 @@
+class Utilidad {
+  constructor(){
+
+  }
+  SoloNumumero(event){
+
+  }
+  //AAAA-MM-DDTFEHCAYHORAZ Retorna DD/MM/AAAA
+  ConvertirFechaHumana(f){
+      fe = f.substr(0,10);
+      fa = fe.split("-");
+      return fa[2] + "/" + fa[1] + "/" + fa[0];
+    }
+  }
+  //DD/MM/AAAA Retorna AAAA-MM-DD
+  ConvertirFechaUnix(sdate){
+
+  }
+  //leer todos los campos de mondo textos
+  ValidarCamposTexto(DIV){
+
+  }
+  //leer todos los campos por select
+  ValidarCamposSelect(DIV){
+
+  }
+  //Obtener la direccion mac
+  ObtenerMAC(MAC){
+
+  }
+  //Obtener la direccion ip
+  ObtenerIP(IP){
+
+  }
+  //Cargar imagenes desde archivos
+  CargarIMG(DIV, URL){
+
+  }
+
+}
