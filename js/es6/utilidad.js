0ecf587682c237b362f0dcd41a0709c03c65b583
Victor Suarez <victormst@gmail.com>
2017-07-21 09:24:32 -0400

    class Utilidad con metodos SoloNumero, ConvertirFechaHumana, ConvertirFechaUnix

--- js/es6/utilidad.js
+++ js/es6/utilidad.js
@@ -2,36 +2,50 @@ class Utilidad {
   constructor(){
 
   }
-  SoloNumumero(event){
-
+  
+  SoloNumero(event){
+          if( event.charCode >= 48 && event.charCode <= 57 )
+            return event.charCode;
+          else
+            return false;
   }
-  //AAAA-MM-DDTFEHCAYHORAZ Retorna DD/MM/AAAA
+  
+  //Recibe  Fecha Formato: AAAA-MM-DD 00:00:00 
+  //Retorna Fecha Formato: DD/MM/AAAA
   ConvertirFechaHumana(f){
       fe = f.substr(0,10);
       fa = fe.split("-");
       return fa[2] + "/" + fa[1] + "/" + fa[0];
-    }
   }
-  //DD/MM/AAAA Retorna AAAA-MM-DD
-  ConvertirFechaUnix(sdate){
+  
 
+  //Recibe  Fecha Formato: DD/MM/AAAA
+  //Retorna Fecha Formato: AAAA-MM-DD
+  ConvertirFechaUnix(f){
+      f = f.split("/");
+      return f[2] + "-" + fa[1] + "-" + fa[0];
   }
+
   //leer todos los campos de mondo textos
   ValidarCamposTexto(DIV){
-
+      //en construccion
   }
+
   //leer todos los campos por select
   ValidarCamposSelect(DIV){
-
+      //en construccion
   }
+
   //Obtener la direccion mac
   ObtenerMAC(MAC){
-
+      //en construccion
   }
+
   //Obtener la direccion ip
   ObtenerIP(IP){
-
+      //en construccion
   }
+  
   //Cargar imagenes desde archivos
   CargarIMG(DIV, URL){
 
