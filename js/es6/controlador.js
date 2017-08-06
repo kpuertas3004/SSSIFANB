f4dacccfca7c810f0d4a56720381b18e7266387d
Carlos E. Peña <gesaodin@gmail.com>
2017-08-06 15:44:49 -0400

    a

--- js/es6/controlador.js
+++ js/es6/controlador.js
@@ -250,3 +250,44 @@ function IniciarSesion(){
 
   }
 }
+
+
+function CargarAPI(sURL, metodo, valores, Objeto){
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.open(metodo, sURL);
+  xhttp.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem('ipsfaToken'));
+
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        if(Objeto != undefined){
+          console.log(xhttp.responseText);
+          Objeto.Crear(JSON.parse(xhttp.responseText));
+        }else{
+          respuesta = JSON.parse(xhttp.responseText);
+          if (respuesta.tipo != 0){
+            $.notify("Se ha Insertado correctamente", "success");
+          }else{
+              alert(xhttp.responseText)
+          }
+        }
+      }
+  }
+  xhttp.onerror = function() {
+      if (this.readyState == 4 && this.status == 0) {
+        $.notify("No se puede conectar al servidor");
+        $("#_cargando").hide();
+          //Pace.stop();
+      }
+
+  };
+
+
+  if(valores != undefined){
+    xhttp.send(JSON.stringify(valores));
+  }else{
+    xhttp.send();
+  }
+
+
+}
